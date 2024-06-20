cfb4f64ea64c23aff2add314c4dfaac311384217
thiranoyama <thiranoyama@gmail.com>
2024-06-20 19:54:02 +0200

    update: Changed login.php

--- bin/cp/session.php
+++ bin/cp/session.php
@@ -2,7 +2,7 @@
 session_start();
 
 if (isset($_SESSION["userid"]) && $_SESSION["userid"] === true) {
-    header("location: /home/britaszk/public_html/welcome.php");
+    header("location: /welcome.php");
     exit;
 }
 ?>
